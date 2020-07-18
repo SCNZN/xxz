22f35d4e5d7cfa1e09ce0d2b04350f4afc8b7a34
vagrant <vagrant@homestead>
2020-07-18 16:37:01 +0000

    修改配置信息

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Tommaso是个胖子
+                     12345678
                 </div>
 
                 <div class="links">
